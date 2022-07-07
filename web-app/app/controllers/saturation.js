3cb1af5c54892abee7a73df98d4499fb99ecbb66
Nick Hageman <nickhageman8@gmail.com>
2022-07-06 22:53:28 -0500

    Changed Post request handling to automically add moisture readings to database

--- web-app/app/controllers/saturation.js
+++ web-app/app/controllers/saturation.js
@@ -16,6 +16,23 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   saturation = req.body.sensorVal;
-  console.log('Saturation Value: ', saturation);
+  mac = req.body.macAddress;
+  //if mac not in dict -> dict[mac] = [saturation] ---else -> if len of dict[mac] > 11 remove the last one and add the new value.
+  console.log('Saturation Value: ', saturation, ' Mac Address: ', mac);
+  //Add moisture to Database
+  async function moistureUpdate() {
+    try {
+    const ID = await Plant.findAll({
+      where: {
+        MAC: mac
+      },
+      });
+      let id = (ID[0].dataValues.id);
+      let data = {plantId: id, moisture: String((((1023-saturation) / 1023) * 100).toFixed(0))};
+      const moisture = await Moisture.create(data);
+    } catch {
+      console.log("Mac Address (", mac, ") not found in Database... Try adding it on the web page");
+    }}
+  moistureUpdate();
   res.json({ message: 'SUCCESS'});
-}) ;
+});
