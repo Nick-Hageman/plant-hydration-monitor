e695853788569eb301c16c7f646c1bef3a78cd33
Nick Hageman <nickhageman8@gmail.com>
2022-07-06 23:09:09 -0500

    Instantiated the Sequelize Database on the server

--- web-app/app.js
+++ web-app/app.js
@@ -1,3 +1,13 @@
+//DB
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: '../../../database/plant.db'
+});
+
+module.exports = sequelize;
+
 var express = require('express'),
   config = require('./config/config');
 
